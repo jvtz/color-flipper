06f86f35fac757c14c9549ceaed324e17d0b6694
jvtz <jvterenciano@protonmail.com>
2021-04-06 10:00:35 -0300

    complete website

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
-function colorSubmitted() {
-  let hexInput = document.getElementById("hex-color");
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+let hexInput = document.getElementById("hex-color");
 
+function randomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
+
+function colorSubmitted() {
   document.body.style.backgroundColor = hexInput.value;
+}
+
+function randomColor() {
+  let hexColor = "#";
+
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[randomNumber()];
+  }
+
+  document.body.style.backgroundColor = hexColor;
+  hexInput.value = hexColor;
 }
